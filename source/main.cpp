ffe9a94cf3cc202092a790e9068edf0fa7a6242e
EBLeifEricson <leifericson96@gmail.com>
2016-07-09 17:55:40 -0400

    Heavily commentated main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -20,6 +20,7 @@
 
 using namespace std;
 
+// Converts string to uppercase characters
 string upperStr(string in) {
 	for (int i = 0; i < in.size(); ++i) {
 		in[i] = toupper(in[i]);
@@ -27,11 +28,13 @@ string upperStr(string in) {
 	return in;
 }
 
+// Converts boolean value to string
 string boolToStr( bool in ) {
 	if (in == true) return "True";
 	else return "False";
 }
 
+// Converts integer value to string
 string intToStr( int num )
 {
 	stringstream ss;
@@ -39,6 +42,8 @@ string intToStr( int num )
 	return ss.str();
 }
 
+
+// Array of note sequences
 string playable[25] = {
 	"zzzzzz",  // NULL
 	"xayxay",  // Zelda's Lullaby
@@ -67,6 +72,7 @@ string playable[25] = {
 	"zzzzzz"  // NULL
 };
 
+// Array of song titles
 string songnames[25] = {
 	"NULL",
 	"Zelda's Lullaby",
@@ -95,6 +101,7 @@ string songnames[25] = {
 	"NULL"
 };
 
+// Detects if a song has been played by the player
 int detectSong(string song) {
 	for (int i = 0; i < sizeof(playable)/sizeof(playable[0]); ++i) {
 		int pos = song.find(playable[i]);
@@ -103,12 +110,14 @@ int detectSong(string song) {
 	return -1;
 }
 
+// Source type
 typedef enum {
 	TYPE_UNKNOWN = -1,
 	TYPE_OGG = 0,
 	TYPE_WAV = 1
 } source_type;
 
+// Source
 typedef struct {
 	source_type type;
 	
@@ -127,6 +136,7 @@ typedef struct {
 
 bool channelList[24];
 
+// Returns the first unused DSP channel available for use
 int getOpenChannel() {
 
 	for (int i = 0; i <= 23; i++) {
@@ -140,6 +150,7 @@ int getOpenChannel() {
 
 }
 
+// Initializes an audio source and returns any errors that may occur
 const char *sourceInit(source *self, const char *filename) {
 	FILE *file = fopen(filename, "rb");
 	if (file) {
@@ -230,6 +241,7 @@ const char *sourceInit(source *self, const char *filename) {
 	return "ok";
 }
 
+// Plays an initialized audio source on its assigned DSP channel
 int sourcePlay(source *self) { // source:play()
 
 	if (self->audiochannel == -1) {
@@ -257,19 +269,23 @@ int sourcePlay(source *self) { // source:play()
 	return self->audiochannel;
 }
 
+// Stop playback of an audio source
 int sourceStop(source *self) { // source:stop()
 	ndspChnWaveBufClear(self->audiochannel);
 	return 0;
 }
 
+// Checks if audio source is playing on its assigned DSP channel
 bool sourceIsPlaying(source *self) { // source:isPlaying()
 	return ndspChnIsPlaying(self->audiochannel);
 }
 
+// Returns duration of audio source in seconds
 double sourceGetDuration(source *self) { // source:getDuration()
 	return (double)(self->nsamples) / self->rate;
 }
 
+// Returns playback position of audio source in seconds
 double sourceTell(source *self) { // source:tell()
 	if (!ndspChnIsPlaying(self->audiochannel)) {
 		return 0;
@@ -278,6 +294,7 @@ double sourceTell(source *self) { // source:tell()
 	}
 }
 
+// Subroutine for when a song is correctly played
 void songSequence(int index) {
 	sftd_font    *font = sftd_load_font_mem(chiaro_otf, chiaro_otf_size);
 	sf2d_texture *bgbot = sfil_load_PNG_buffer(bgbottom_png, SF2D_PLACE_RAM);
@@ -329,13 +346,17 @@ void songSequence(int index) {
 
 int main(int argc, char* argv[])
 {
+	// Seed rand
 	srand(time(NULL));
 	
+	// Init graphics
 	sf2d_init();
 	sf2d_set_clear_color(RGBA8(0x40, 0x40, 0x40, 0xFF));
 	sf2d_set_3D(0);
-	
+	// Init font
 	sftd_init();
+	
+	// Load fonts & textures
 	sftd_font    *font = sftd_load_font_mem(chiaro_otf, chiaro_otf_size);
 	sf2d_texture *bgbot = sfil_load_PNG_buffer(bgbottom_png, SF2D_PLACE_RAM);
 	sf2d_texture *oot = sfil_load_PNG_buffer(ocarina_png, SF2D_PLACE_RAM);
@@ -346,11 +367,7 @@ int main(int argc, char* argv[])
 	ndspInit();
 	ndspSetOutputCount(23);
 	
-	string playingsong = "";
-	int currnote = 0;
-	
-	int playRes = -1;
-	
+	// Audio sources
 	source* ocarina1 = new source;
 	sourceInit(ocarina1, "sdmc:/3ds/orchestrina/data/OOT_Notes_Ocarina_D_long.wav");
 	source* ocarina2 = new source;
@@ -364,16 +381,23 @@ int main(int argc, char* argv[])
 	source* correct = new source;
 	sourceInit(correct, "sdmc:/3ds/orchestrina/data/OOT_Song_Correct.wav");
 	
-	int textWidth = 0;
-	int textHeight = 0;
+	// Last 20 played notes
+	string playingsong = "";
 	
+	// Debug result of playing audio
+	int playRes = -1;
+	
+	// Index of song played
+	int songtrigger = -1;
+
+	// Button press bools for highlighting
 	bool pressedL = false; // D 
 	bool pressedX = false; // B
 	bool pressedY = false; // A
 	bool pressedA = false; // D2
 	bool pressedR = false; // F
 	
-	int songtrigger = -1;
+	
 	
 	while(aptMainLoop())
 	{
@@ -381,102 +405,110 @@ int main(int argc, char* argv[])
 		
 		u32 keys = hidKeysDown();
 		u32 released = hidKeysUp();
-		
+	
+		// Exit on Start
 		if(keys & KEY_START)
 			break;
 		
+		// Play notes on button presses
 		if((keys & KEY_L) && !pressedL) {
 			pressedL = true;
-			
 			playRes = sourcePlay(ocarina1);
 			playingsong.push_back('l');
-			++currnote;
 		}
 		
 		if((keys & KEY_X) && !pressedX) {
 			pressedX = true;
-			
 			playRes = sourcePlay(ocarina2);
 			playingsong.push_back('x');
-			++currnote;
 		}
 		
 		if((keys & KEY_Y) && !pressedY) {
 			pressedY = true;
 			playRes = sourcePlay(ocarina3);
 			playingsong.push_back('y');
-			++currnote;
 		}
 		
 		if((keys & KEY_A) && !pressedA) {
 			pressedA = true;
-			
 			playRes = sourcePlay(ocarina4);
 			playingsong.push_back('a');
-			++currnote;
 		}
 		
 		if((keys & KEY_R) && !pressedR) {
 			pressedR = true;
-			
+
 			playRes = sourcePlay(ocarina5);
 			playingsong.push_back('r');
-			++currnote;
 		}
 		
+		// Check for releases
 		if ((released & KEY_L) && pressedL) pressedL = false;
 		if ((released & KEY_X) && pressedX) pressedX = false;
 		if ((released & KEY_Y) && pressedY) pressedY = false;
 		if ((released & KEY_A) && pressedA) pressedA = false;
 		if ((released & KEY_R) && pressedR) pressedR = false;
 		
+		// Start top screen
 		sf2d_start_frame(GFX_TOP, GFX_LEFT);
 
 			sf2d_draw_rectangle(0, 0, 400, 240, RGBA8(0, 0, 0, 255));
-			sftd_draw_text(font, 5, 20, RGBA8(255, 255, 255, 255), 24, upperStr(playingsong).c_str());
-			sftd_draw_text(font, 5, 50, RGBA8(255, 255, 255, 255), 12, ("Channel: "+intToStr(playRes)).c_str());
-			string play = "L: "+boolToStr(sourceIsPlaying(ocarina1))+" R: "+boolToStr(sourceIsPlaying(ocarina2))+" X: "+boolToStr(sourceIsPlaying(ocarina3))+\
-			" Y: "+boolToStr(sourceIsPlaying(ocarina4))+" A: "+boolToStr(sourceIsPlaying(ocarina5));
-			sftd_draw_text(font, 5, 70, RGBA8(255, 255, 255, 255), 12, (play.c_str()));
+			
+			//Debug stuff
+			sftd_draw_text(font, 5, 20, RGBA8(255, 255, 255, 255), 24, upperStr(playingsong).c_str()); // Last 20 notes played
+			sftd_draw_text(font, 5, 50, RGBA8(255, 255, 255, 255), 12, ("Channel: "+intToStr(playRes)).c_str()); // Audio channel of last sound played
+			string play = "L: "+boolToStr(sourceIsPlaying(ocarina1))+" X: "+boolToStr(sourceIsPlaying(ocarina2))+" Y: "+boolToStr(sourceIsPlaying(ocarina3))+\
+			" A: "+boolToStr(sourceIsPlaying(ocarina4))+" R: "+boolToStr(sourceIsPlaying(ocarina5)); 
+			sftd_draw_text(font, 5, 70, RGBA8(255, 255, 255, 255), 12, (play.c_str())); // Status of each button's sound
 
 		sf2d_end_frame();
 		
+		// Start bottom screen
 		sf2d_start_frame(GFX_BOTTOM, GFX_LEFT);
 			sf2d_draw_texture(bgbot, 0, 0);
 			sf2d_draw_texture(itemblock, 10, 10);
 			sf2d_draw_texture(itemblock, 75, 10);
 			sf2d_draw_texture(oot, 15, 15);
 			sf2d_draw_texture(buttons, 100, 100);
+			
+			// Red highlights when buttons pressed
 			if (pressedL) sf2d_draw_rectangle(100, 100, 24, 26, RGBA8(0xFF, 0x00, 0x00, 0x7F));
 			if (pressedR) sf2d_draw_rectangle(124, 100, 24, 26, RGBA8(0xFF, 0x00, 0x00, 0x7F));
 			if (pressedX) sf2d_draw_rectangle(148, 100, 24, 26, RGBA8(0xFF, 0x00, 0x00, 0x7F));
 			if (pressedY) sf2d_draw_rectangle(172, 100, 24, 26, RGBA8(0xFF, 0x00, 0x00, 0x7F));
 			if (pressedA) sf2d_draw_rectangle(196, 100, 24, 26, RGBA8(0xFF, 0x00, 0x00, 0x7F));
 		sf2d_end_frame();
-		
-		if (playingsong.size() > playingsong.max_size()) playingsong = "";
 
+		// If song index is valid
 		if (songtrigger != -1) {
-			
+			// Prevent button highlights from getting stuck
+			pressedL = false; pressedX = false; pressedY = false; pressedA = false; pressedR = false;
 			playRes = sourcePlay(correct);
+			// Clear played notes
 			playingsong = "";
 			sf2d_swapbuffers();
 			svcSleepThread(1500000000);
-			hidScanInput();
+			// Play the song that was entered
 			songSequence(songtrigger);
-			hidInit();
 		}
+		
+		// Update song index
 		songtrigger = detectSong(playingsong);
 		
+		// Keep note history short
 		if (playingsong.size() > 20) {
 			playingsong = playingsong.substr(playingsong.size()-20, 20);
 		}
 		
+		// Swap buffers
 		sf2d_swapbuffers();
 	}
 	
+	// Free stuff
 	sftd_free_font(font);
+	// TODO: Free everything up correctly
 	
+	// Exit services
 	ndspExit();
 	sf2d_fini();
 	sftd_fini();
